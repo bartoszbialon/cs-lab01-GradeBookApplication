84fb10315e5cdc76aeaaa012182206bdafa693b2
bartoszbialon <bartosz.bialon@microsoft.wsei.edu.pl>
2023-03-18 14:52:27 +0100

    Adding Type property to BaseGradeBook

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace GradeBook.GradeBooks
 {
@@ -13,6 +14,7 @@ namespace GradeBook.GradeBooks
     {
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public GradeBookType Type { get; set; }
 
         public BaseGradeBook(string name)
         {
