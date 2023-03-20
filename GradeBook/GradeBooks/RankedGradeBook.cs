09ea390840389db5488dcc417e6dbf6c58baef56
bartoszbialon <bartosz.bialon@microsoft.wsei.edu.pl>
2023-03-20 19:27:26 +0100

    Overriding RankedGradeBook's CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -83,5 +83,19 @@ namespace GradeBook.GradeBooks
             }
         }
 
+        //Overriding RankedGradeBook's CalculateStudentStatistics method.
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+            }
+
+            else
+            {
+                base.CalculateStudentStatistics(name);
+            }
+        }
+
     }
 }
