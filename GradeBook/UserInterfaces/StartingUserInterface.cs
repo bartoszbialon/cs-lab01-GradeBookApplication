b8966939c971da6c799829c1f7245063bc8d1b8f
bartoszbialon <bartosz.bialon@microsoft.wsei.edu.pl>
2023-03-22 15:45:08 +0100

    Updating HelpCommand in StartingUserInterface class

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -90,12 +90,13 @@ namespace GradeBook.UserInterfaces
         }
 
         //Updating StartingUserInterfaces's HelpCommand method.
+        //Final update to StartingUserInterface's HelpCommand method.
         public static void HelpCommand()
         {
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
+            Console.WriteLine("Create 'Name' 'Type' 'Weighted' - Creates a new gradebook where 'Name' is the name of the gradebook, 'Type' is what type of grading it should use, and 'Weighted' is whether or not grades should be weighted (true or false).");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
